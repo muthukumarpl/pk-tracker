7e62d72b4c079549f81dd4f06ac14cf22fd4e437
muthukumarpl <77067221+muthukumarpl@users.noreply.github.com>
2025-12-20 17:13:37 +0530

    Add files via upload

---
+++ expenses/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('set-budget/', views.set_budget, name='set_budget'), # 🔥 NEW
+    path('expenses/', views.expense_list, name='expense_list'),
+    path('edit/<int:id>/', views.edit_expense, name='edit_expense'),
+    path('delete/<int:id>/', views.delete_expense, name='delete_expense'),
+    path('charts/', views.charts, name='charts'),
+    path('download/', views.download, name='download'),
+    path('export-csv/', views.export_csv, name='export_csv'),
+]
